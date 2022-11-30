25b7380e1e84ce2a2322ae7d37072bf87e2b643f
palmally <palmally@yandex.ru>
2022-11-30 23:09:33 +0300

    feat: complete quizz logic

--- src/blocks/quizz-form/quizz-form.js
+++ src/blocks/quizz-form/quizz-form.js
@@ -3,16 +3,25 @@ const quizz = document.querySelector('.quizz-form'),
   firstQuizzScreen = quizz.querySelector('.quizz-form__screen_number_first'),
   secondQuizzScreen = quizz.querySelector('.quizz-form__screen_number_second'),
   nextSreenButton = firstQuizzScreen.querySelector('.quizz-form__button_type_next-screen'),
-  screenVisibilityClass = 'quizz-form__screen_visible'
+  screenVisibilityClass = 'quizz-form__screen_visible',
+  formVisibilityClass = 'quizz-form_visible',
+  resultVisibilityClass = 'quizz-result_visible',
+  mentorScreenSelector = '.quizz-result_type_mentor',
+  reviewScreenSelector = '.quizz-result_type_review',
+  duoScreenSelector = '.quizz-result_type_duo'
 
 const changeQuizzScreen = () => {
   firstQuizzScreen.classList.remove(screenVisibilityClass);
   secondQuizzScreen.classList.add(screenVisibilityClass);
 }
 
+const showQuizzResultScreen = (selector) => {
+  quizz.classList.remove(formVisibilityClass);
+  document.querySelector(selector).classList.add(resultVisibilityClass);
+}
+
 // считаем выбранные чекбоксы
-const completeQuiz = (e) => {
-  e.preventDefault();
+const getQuizResult = () => {
   const number = [...quizForm.elements].reduce((
     res,
     { checked, dataset: { answer }}
@@ -22,8 +31,22 @@ const completeQuiz = (e) => {
     }
     return res
   }, { mentor: 0, review: 0 });
-  console.log(number);
+  return number;
 }
+
+const completeQuiz = (e) => {
+  e.preventDefault();
+  const { mentor, reviev } = getQuizResult();
+  if (mentor > reviev) {
+    showQuizzResultScreen(mentorScreenSelector)
+    return;
+  }
+  if (mentor < reviev) {
+    showQuizzResultScreen(reviewScreenSelector)
+    return;
+  }
+  showQuizzResultScreen(duoScreenSelector);
+};
 
 nextSreenButton.addEventListener('click', changeQuizzScreen);
 quizForm.addEventListener('submit', completeQuiz);
