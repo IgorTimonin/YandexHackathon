bb0fef2b3c6975315476f7dd59e83236e982239f
Igor Timonin <mut4nt@yandex.ru>
2022-11-30 13:06:24 +0300

    добавлено появление карточек

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ const numbers = [2, 3, 5];
 const doubledNumbers = numbers.map(number => number * 2);
 
 console.log(doubledNumbers);
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add('what-give__show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.what-give__animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
