03e702a429d3ccca1c63642c3014160b5faa1550
palmally <palmally@yandex.ru>
2022-12-02 00:06:53 +0300

    fix:texts

--- src/index.js
+++ src/index.js
@@ -17,19 +17,19 @@ const slider = new Slider({
 slider.enable();
 
 //Появление сообщений при скролле в разделе Что Яндекс Практикум может дать тебе
-// function onEntry(entry) {
-//   entry.forEach((change) => {
-//     if (change.isIntersecting) {
-//       change.target.classList.add('what-give__show');
-//     }
-//   });
-// }
-// let options = { threshold: [0.5] };
-// let observer = new IntersectionObserver(onEntry, options);
-// let elements = document.querySelectorAll('.what-give__animation');
-// for (let elm of elements) {
-//   observer.observe(elm);
-// };
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add('what-give__show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.what-give__animation');
+for (let elm of elements) {
+  observer.observe(elm);
+};
 
 const quizzPopup = new Popup({
   popupSelector: '.popup_type_quizz',
